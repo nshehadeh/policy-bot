df48d1b8acf4dc1a877758dddcc96a10b182834c
Nishan Shehadeh <59905133+nshehadeh@users.noreply.github.com>
2024-08-13 13:53:03 -0400

    Update urls.py

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -3,5 +3,8 @@ from .views import UserCreate, ChatView
 
 urlpatterns = [
     path('users/', UserCreate.as_view(), name='user_create'),
+    path('user_settings/', UserSettingsView.as_view(), name='user_details'),  # Endpoint for user details and settings
     path('chat/', ChatView.as_view(), name='chat'),
+    path('chat/sessions/', LoadPreviousChatView.as_view(), name='chat_sessions'),  # Fetch all sessions
+    path('chat/load/', LoadPreviousChatView.as_view(), name='load_chat'),  # Load a specific session's chat history
 ]
