d852443fcadc0642e764de6743804fd25ac572e7
Nishan Shehadeh <59905133+nshehadeh@users.noreply.github.com>
2024-08-13 13:53:40 -0400

    Update urls.py

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserCreate, ChatView
+from .views import UserCreate, ChatView, LoadPreviousChatView, UserSettingsView
 
 urlpatterns = [
     path('users/', UserCreate.as_view(), name='user_create'),
